db1c7977cf5d28992993b604ee520123ff63a479
BiteBliss <156935513+BiteBliss@users.noreply.github.com>
2026-01-13 21:51:13 +0800

    Fix import paths

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,6 @@
 import { IncomingForm } from "formidable";
-import { processUpload } from "../../lib/processUpload";
-import { predictImage } from "../../lib/pawIdPredict";
+import processUpload from '../lib/processUpload.js';
+import { predictImage } from "../../lib/pawIdPredict.js";
 
 export const config = {
   api: {
